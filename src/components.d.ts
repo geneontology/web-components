07b35d8d55eb5cddedca4166709391af9594f950
Patrick Kalita <pkalita@lbl.gov>
2024-12-06 16:37:53 -0800

    Add go-autocomplete component

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,16 +5,78 @@
  * It contains typing information for all components that exist in this project.
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
-export namespace Components {}
+export namespace Components {
+    interface WcGoAutocomplete {
+        /**
+          * Category to constrain the search; by default search "gene" Other values accepted: `undefined` : search both terms and genes `gene` : will only search genes used in GO `biological%20process` : will search for GO BP terms `molecular%20function` : will search for GO MF terms `cellular%20component` : will search for GO CC terms `cellular%20component,molecular%20function,biological%20process` : will search any GO term
+         */
+        "category": string;
+        /**
+          * Maximum number of results to show
+         */
+        "maxResults": number;
+        /**
+          * Default placeholder for the autocomplete
+         */
+        "placeholder": string;
+        "value": string;
+    }
+}
+export interface WcGoAutocompleteCustomEvent<T> extends CustomEvent<T> {
+    detail: T;
+    target: HTMLWcGoAutocompleteElement;
+}
 declare global {
-  interface HTMLElementTagNameMap {}
+    interface HTMLWcGoAutocompleteElementEventMap {
+        "itemSelected": any;
+    }
+    interface HTMLWcGoAutocompleteElement extends Components.WcGoAutocomplete, HTMLStencilElement {
+        addEventListener<K extends keyof HTMLWcGoAutocompleteElementEventMap>(type: K, listener: (this: HTMLWcGoAutocompleteElement, ev: WcGoAutocompleteCustomEvent<HTMLWcGoAutocompleteElementEventMap[K]>) => any, options?: boolean | AddEventListenerOptions): void;
+        addEventListener<K extends keyof DocumentEventMap>(type: K, listener: (this: Document, ev: DocumentEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
+        addEventListener<K extends keyof HTMLElementEventMap>(type: K, listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any, options?: boolean | AddEventListenerOptions): void;
+        addEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | AddEventListenerOptions): void;
+        removeEventListener<K extends keyof HTMLWcGoAutocompleteElementEventMap>(type: K, listener: (this: HTMLWcGoAutocompleteElement, ev: WcGoAutocompleteCustomEvent<HTMLWcGoAutocompleteElementEventMap[K]>) => any, options?: boolean | EventListenerOptions): void;
+        removeEventListener<K extends keyof DocumentEventMap>(type: K, listener: (this: Document, ev: DocumentEventMap[K]) => any, options?: boolean | EventListenerOptions): void;
+        removeEventListener<K extends keyof HTMLElementEventMap>(type: K, listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => any, options?: boolean | EventListenerOptions): void;
+        removeEventListener(type: string, listener: EventListenerOrEventListenerObject, options?: boolean | EventListenerOptions): void;
+    }
+    var HTMLWcGoAutocompleteElement: {
+        prototype: HTMLWcGoAutocompleteElement;
+        new (): HTMLWcGoAutocompleteElement;
+    };
+    interface HTMLElementTagNameMap {
+        "wc-go-autocomplete": HTMLWcGoAutocompleteElement;
+    }
 }
 declare namespace LocalJSX {
-  interface IntrinsicElements {}
+    interface WcGoAutocomplete {
+        /**
+          * Category to constrain the search; by default search "gene" Other values accepted: `undefined` : search both terms and genes `gene` : will only search genes used in GO `biological%20process` : will search for GO BP terms `molecular%20function` : will search for GO MF terms `cellular%20component` : will search for GO CC terms `cellular%20component,molecular%20function,biological%20process` : will search any GO term
+         */
+        "category"?: string;
+        /**
+          * Maximum number of results to show
+         */
+        "maxResults"?: number;
+        /**
+          * Event triggered whenever an item is selected from the autocomplete
+         */
+        "onItemSelected"?: (event: WcGoAutocompleteCustomEvent<any>) => void;
+        /**
+          * Default placeholder for the autocomplete
+         */
+        "placeholder"?: string;
+        "value"?: string;
+    }
+    interface IntrinsicElements {
+        "wc-go-autocomplete": WcGoAutocomplete;
+    }
 }
 export { LocalJSX as JSX };
 declare module "@stencil/core" {
-  export namespace JSX {
-    interface IntrinsicElements {}
-  }
+    export namespace JSX {
+        interface IntrinsicElements {
+            "wc-go-autocomplete": LocalJSX.WcGoAutocomplete & JSXBase.HTMLAttributes<HTMLWcGoAutocompleteElement>;
+        }
+    }
 }
