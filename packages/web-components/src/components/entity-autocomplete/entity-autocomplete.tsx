a32adbe98ffd1f029f9b6a6e3912ea4e7e2c89d7
Patrick Kalita <pkalita@lbl.gov>
2025-07-24 12:30:31 -0700

    Use shadow DOM for EntityAutocomplete

--- packages/web-components/src/components/entity-autocomplete/entity-autocomplete.tsx
+++ packages/web-components/src/components/entity-autocomplete/entity-autocomplete.tsx
@@ -11,7 +11,7 @@ import * as dbxrefs from "@geneontology/dbxrefs";
 @Component({
   tag: "go-entity-autocomplete",
   styleUrl: "entity-autocomplete.css",
-  shadow: false,
+  shadow: true,
 })
 export class EntityAutocomplete {
   searchBox: HTMLInputElement;
@@ -123,7 +123,7 @@ export class EntityAutocomplete {
       ""
     ) : (
       <div class="autocomplete">
-        <i class="fas fa-search icon-left"></i>
+        <span class="icon icon-left">{this.renderSearchIcon()}</span>
         <input
           type="text"
           class="input-field"
@@ -132,14 +132,15 @@ export class EntityAutocomplete {
           ref={(el) => (this.searchBox = el as HTMLInputElement)}
           onInput={(evt) => this.newSearch(evt)}
         ></input>
-        <i
-          class="far fa-times-circle icon-right"
+        <span
+          class="icon icon-right"
           onClick={() => {
             this.docs = undefined;
             this.value = "";
           }}
-        ></i>
-
+        >
+          {this.renderClearIcon()}
+        </span>
         {!this.docs ? (
           ""
         ) : (
@@ -193,6 +194,35 @@ export class EntityAutocomplete {
           }
         </span>
       </div>
+    );
+  }
+
+  renderSearchIcon() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        viewBox="0 0 16 16"
+      >
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+      </svg>
+    );
+  }
+
+  renderClearIcon() {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        viewBox="0 0 16 16"
+      >
+        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+      </svg>
     );
   }
 }
